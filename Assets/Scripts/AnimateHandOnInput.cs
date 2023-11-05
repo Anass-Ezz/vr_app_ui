5813ecace4e407e3a996d21175f44787bf924d84
belkadi anass <anassbelkadi70@gmail.com>
2023-11-05 15:02:28 +0100

    adding ui for gaze

--- Assets/Scripts/AnimateHandOnInput.cs
+++ Assets/Scripts/AnimateHandOnInput.cs
@@ -7,7 +7,6 @@ public class AnimateHandOnInput : MonoBehaviour
 {
     public InputActionProperty pinchAnimatorAction;
     public InputActionProperty gripAnimatorAction;
-    public InputActionProperty A_button_action;
     public Animator handAnimator;
     // Start is called before the first frame update
     void Start()
@@ -20,7 +19,6 @@ public class AnimateHandOnInput : MonoBehaviour
     {
         float pinchValue = pinchAnimatorAction.action.ReadValue<float>();
         float gripValue = gripAnimatorAction.action.ReadValue<float>();
-        float A_Value = A_button_action.action.ReadValue<float>();
         handAnimator.SetFloat("Trigger", pinchValue);
         handAnimator.SetFloat("Grip", gripValue);
     }
--- Assets/Scripts/EventsHandeler.cs
+++ Assets/Scripts/EventsHandeler.cs
@@ -1,34 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class EventsHandeler : MonoBehaviour
 {
-    public LayerMask newLayerMask;
+
+
+    public InputActionProperty triggerAction;
+    [SerializeField] private LayerMask newLayerMask;
     [SerializeField] private Camera camera;
-    public void Gaze(GameObject gameObject){
-        
+    private GameObject gazedObject;
+    private GameObject duplicatedGazedObject;
+    private bool hover = false;
+    private bool objectGazed = false;
+    public Color backgroundColor = Color.black;
+    [SerializeField] GameObject descriptionUI;
+   
+    public void Gaze(){
+        objectGazed = true;
         Vector3 camera_position = camera.transform.position;
         Quaternion zeroRotation = Quaternion.identity;
-        GameObject duplicatedObject = Instantiate(gameObject, camera_position, zeroRotation);
-        Rigidbody rb = duplicatedObject.GetComponent<Rigidbody>();
-        Component XR_intr_script = duplicatedObject.GetComponent("XRSimpleInteractable");
+        GameObject duplicatedGazedObject = Instantiate(gazedObject, camera_position + new Vector3(0f, 0f, 2f), zeroRotation);
+        descriptionUI.transform.position = camera_position + new Vector3(-1.5f, 0f, 2f);
+        // descriptionUI.transform.rotation = camera.transform.rotation;
+        Rigidbody rb = duplicatedGazedObject.GetComponent<Rigidbody>();
+        Component XR_intr_script = duplicatedGazedObject.GetComponent("XRSimpleInteractable");
         XR_intr_script.GetType().GetProperty("enabled").SetValue(XR_intr_script, false, null);
         rb.useGravity = false;
         rb.isKinematic = true;
-        duplicatedObject.layer = LayerMask.NameToLayer("Gaze");
-        camera.cullingMask = newLayerMask;
-        //dskdjk
+        duplicatedGazedObject.layer = LayerMask.NameToLayer("Gaze");
+        camera.cullingMask = LayerMask.GetMask("Gaze", "player");
+        camera.clearFlags = CameraClearFlags.SolidColor;
+        camera.backgroundColor = backgroundColor;
     }
-    // Start is called before the first frame update
+
+    public void OnHoverEntered(GameObject gameObject){
+        if (!this.hover) this.hover = !this.hover;
+        gazedObject = gameObject;
+    }
+    public void OnHoverExited(GameObject gameObject){
+        if (this.hover) this.hover = !this.hover;
+        gazedObject = gameObject;
+    }
+    
+    public void DescExitButton(){
+        Destroy(gazedObject);
+        camera.cullingMask = LayerMask.GetMask("Default", "TransparentFX", "Ignore Raycast", "Water", "UI", "player");
+    }
+
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        float triggerValue = triggerAction.action.ReadValue<float>();    
+        if (this.hover && triggerValue > 0 && !objectGazed){
+            Gaze();
+        }
+        Debug.Log("Hover : ");
+        Debug.Log(hover);
+        Debug.Log("trigger value : ");
+        Debug.Log(triggerValue);
     }
 }
